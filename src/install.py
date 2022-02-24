21497277d52c6c289b29ce4299e1383e67d7f291
Mark Newman <mark@trinetteandmark.com>
2022-02-24 16:22:51 -0500

    cleanup

--- src/install.py
+++ src/install.py
@@ -3,5 +3,4 @@ from setuptools.command.install import install
 
 class InstallOverride(install):
     def run(self):
-        super().run()        
-        nltk.download('punkt')
+        super().run()
